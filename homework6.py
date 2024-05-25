9020fe43947f6f621d84dec5132b3c579843c74e
Yarik72 <lazutinyaroslav2021@gmail.com>
2024-05-25 23:15:31 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {"Yaroslav":1993,"Igor":2003,"Dima":1995}
+print(my_dict)
+print(my_dict.get("Dima"))
+print(my_dict.get("Victor"))
+my_dict.update({"Misha":1998,"Ruslan":1999})
+a = my_dict.pop(('Igor'))
+print(a)
+print(my_dict)
+print('\n')
+my_set = {5,6,9,5,9,4,3,5,6,6,True,'blue',True,'blue'}
+print(my_set)
+my_set.add((88,99,100))
+my_set.add(7)
+my_set.remove(3)
+print(my_set)
